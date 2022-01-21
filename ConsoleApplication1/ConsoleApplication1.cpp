d3b26bd20349808c2034b7ae0f2fdd23857b5873
Spep <ilyaesegj@gmail.com>
2022-01-21 19:21:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+double Eps;
+double f(double x)
+{
+    return 7 * pow(sin(x), 2);
+}
+double minF(double s, double e)
+{
+    double min = (s + e) / 2;
+    if (e - s < Eps)
+    return min;
+
+    double x1 = minF(s, min), x2 = minF(min, e);
+    return f(x1) < f(x2) ? x1 : x2;
+}
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    cout << "Задайте точность(Eps):";
+    cin >> Eps;
+    cout << minF(2, 6);
+}
